f0a6addd6cfc204b4ce628bbad05709cae78b629
Rishab Lath <reshablath@gmail.com>
2024-09-24 20:44:22 +0530

    Installed Dependencies and basic setup

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,9 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="flex justify-center items-center w-full min-h-screen">
+    <div className="flex justify-center items-center w-full min-h-screen flex-col">
       <h1>Hyy Guys! Lets Create a Blog SAAS App</h1>
+      <h2>Installed Major Dependencies and Connected it with API keys</h2>
     </div>
   );
 }
