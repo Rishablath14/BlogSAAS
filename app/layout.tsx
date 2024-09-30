7ca508b8c322bb3af4381e4291f0fe726309254e
Rishab Lath <reshablath@gmail.com>
2024-09-30 21:11:02 +0530

    third trial

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,7 +27,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className="scroll-smooth">
+    <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable} scroll-smooth`}>
       <ThemeProvider
             attribute="class"
